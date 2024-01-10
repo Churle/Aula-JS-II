056cd71e5c3779032ad13cbd2d1d2c713b3d10ae
ChurMaster <charlie_cwv@hotmail.com>
2024-01-09 23:12:59 -0300

    test1 done

--- Teste1/teste1.js
+++ Teste1/teste1.js
@@ -1,13 +1,13 @@
-let numeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let numeros = prompt('Digite a quantidade de notas para o cálculo da média:');
 let somatoria = 0;
 let contador = numeros;
 
 while(contador > 0){
-    let numero = parseInt(prompt('Digite o numero:'));
-    soma += numero;contador --;
+    let notas = parseInt(prompt('Digite a nota:'));
+    somatoria += notas;contador --;
     
 }
 
 let media = somatoria / numeros;
-
 console.log(media);
+alert(`Sua media e:  ${media}`)
